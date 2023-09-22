70ccaf50a6bcb6965f249e3806ebd176d7bd55f6
sak23042006 <138766433+sak23042006@users.noreply.github.com>
2023-09-22 10:13:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-strings
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-csbm16)
---
+++ script.js
@@ -0,0 +1,34 @@
+// Challenge 1: Store the string "I am a" in one variable and store the other string "Kalvian" in other variable and concatenate both the string to get the output as "I am a Kalvain" using "+" operator.
+var a = 'I am a ';
+var b = 'Kalvian';
+console.log(a + b);
+
+// Challenge 2: Store the given string in a variable and print the same in console. String: If you fail, never give up because FAIL means "First Attempt In Learning".
+var c =
+  'If you fail, never give up because FAIL means "First Attempt In Learning"';
+console.log(c);
+
+// Challenge 3: Store a string in a variable and display the length of the string.
+d = 'arunkumar';
+console.log(d.length);
+
+// Challenge 4: Store a string in a variable and convert it into uppercase.
+e = 'sak';
+console.log(e.toUpperCase());
+
+//Challenge 5: Store the string "I love programming" in a variable and replace the word programming with "JavaScript programming".
+f = 'I love programming';
+console.log(f.replace('programming', 'JavaScript programming'));
+
+// Challenge 6: Store a string in variable and print the string in console 3 times using repeat method.
+g = 'Arun ';
+console.log(g.repeat(3));
+
+//Challenge 7: Store the string as "I am a Kalvian" in a variable and convert the string into array and print the array in console.
+
+
+// Challenge 8: Store the given string "Time and Tide wait for none" in a variable and using indexOf method find the index of "Ti" and print the same in console.
+
+// Challenge 9: Store the given string "When life gives you lemons make lemonade" in a variable and find whether it includes "lemon".
+
+// Challenge 10: Store the given string "You must be the change you wish to see in the world." in a variable and use slice to console log the word "change" and use substring method to console log the word "world".
